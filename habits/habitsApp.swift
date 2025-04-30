f687cd0ecc72e5177b312a223640c411fac77a07
yousseflaamim <yousseflamin99@gmail.com>
2025-04-30 12:19:45 +0200

    Initial Commit

---
+++ habits/habitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitsApp.swift
+//  habits
+//
+//  Created by gio on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct habitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ habitsTests/habitsTests.swift
@@ -0,0 +1,17 @@
+//
+//  habitsTests.swift
+//  habitsTests
+//
+//  Created by gio on 4/30/25.
+//
+
+import Testing
+@testable import habits
+
+struct habitsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
