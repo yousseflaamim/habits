5c5233ee99c7173071d3d37960d9234af1782169
yousseflaamim <yousseflamin99@gmail.com>
2025-04-30 14:56:48 +0200

    add firbase

--- habits/App/habitsApp.swift
+++ habits/App/habitsApp.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct habitsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate : NSObject , UIApplicationDelegate{
+    func application (_application: UIApplication, didFinishLaunchingWithOptions launchOption : [UIApplication.LaunchOptionsKey: Any]?)-> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
